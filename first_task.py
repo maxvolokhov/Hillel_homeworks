69001d153ff5b15cae98dbc0f718c7a964043edf
Max Volokhov <max.volokhov15@gmail.com>
2023-05-09 20:03:33 +0300

    Task fixed

--- first_task.py
+++ first_task.py
@@ -1,4 +1,6 @@
 eleks = ['John', 'Viktor', 'Olha', 'Abraham']
 toshiba = ['John', 'Peter', 'Aleksandr', 'Julia']
-eleks.append(toshiba)
-# or eleks.extend(toshiba)
+toshiba.extend(eleks)
+eleks.clear()
+print(toshiba)
+print(eleks)
