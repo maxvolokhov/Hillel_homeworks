c29c07871d0d83e391a8d0b23590e753450c24f1
Max Volokhov <max.volokhov15@gmail.com>
2023-05-16 15:23:28 +0300

    First task done

---
+++ HW4/task_1.py
@@ -0,0 +1,14 @@
+str_data = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
+str_data = str_data.strip()
+pairs = str_data.split('&')
+result_dict = {}
+for pair in pairs:
+    split_pair = pair.split('=')
+    if len(split_pair) == 2:
+        key, value = split_pair
+        if value.isdigit():
+            result_dict[key] = int(value)
+        else:
+            result_dict[key] = value
+
+print(result_dict)
